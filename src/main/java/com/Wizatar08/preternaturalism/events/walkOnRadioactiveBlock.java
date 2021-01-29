c8c1e77b121a73168158fda8f07c883ee030b66d
_Wizatar08 <kiyoshijim88@gmail.com>
2021-01-29 00:47:53 -0700

    Updated WalkOnRadioactiveBlock class

--- src/main/java/com/Wizatar08/preternaturalism/events/walkOnRadioactiveBlock.java
+++ src/main/java/com/Wizatar08/preternaturalism/events/walkOnRadioactiveBlock.java
@@ -3,6 +3,7 @@ package com.inf1n1T388.preternaturalism.events;
 import com.inf1n1T388.preternaturalism.Preternaturalism;
 import com.inf1n1T388.preternaturalism.init.BlockInit;
 import com.inf1n1T388.preternaturalism.init.ModDamageSource;
+import com.inf1n1T388.preternaturalism.init.ModEntityTypes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
@@ -22,12 +23,9 @@ public class walkOnRadioactiveBlock {
         World world = livingEntity.getEntityWorld();
         if ((world.getBlockState(livingEntity.getPosition().add(0, -1, 0)) == BlockInit.CONTAMINATED_SOIL.get().getDefaultState()) || (world.getBlockState(livingEntity.getPosition().add(0, -1, 0)) == BlockInit.CONTAMINATED_STONE.get().getDefaultState()) || (world.getBlockState(livingEntity.getPosition().add(0, -1, 0)) == BlockInit.CONTAMINATED_OBSIDIAN.get().getDefaultState())) {
             if(livingEntity.world.getGameRules().getBoolean(Preternaturalism.contaminatedBlocksEffects)) {
-
-                livingEntity.attackEntityFrom(ModDamageSource.CONTAMINATED_BLOCK, 5.0F);
-                /*livingEntity.addPotionEffect(new EffectInstance(Effects.WITHER, 600, 4));
-                livingEntity.addPotionEffect(new EffectInstance(Effects.SLOWNESS, 600, 2));
-                livingEntity.addPotionEffect(new EffectInstance(Effects.NAUSEA, 200, 0));
-                livingEntity.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 100, 0));*/
+                if (livingEntity.getType() != ModEntityTypes.MUTATED_SPIDER.get()) {
+                    livingEntity.attackEntityFrom(ModDamageSource.CONTAMINATED_BLOCK, 5.0F);
+                }
             }
         }
     }
